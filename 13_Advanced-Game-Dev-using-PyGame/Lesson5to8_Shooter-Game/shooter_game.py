5392e2fd850d8f6a43fa6d9237cef51493c2867f
ajgquional <ajquional@gmail.com>
2026-06-02 10:10:31 +0800

    Update: shooter_game.py to use Arial for font2

--- 13_Advanced-Game-Dev-using-PyGame/Lesson5to8_Shooter-Game/shooter_game.py
+++ 13_Advanced-Game-Dev-using-PyGame/Lesson5to8_Shooter-Game/shooter_game.py
@@ -12,7 +12,8 @@ font1 = font.Font(None, 80)
 win = font1.render('YOU WIN!', True, (255, 255, 255))
 lose = font1.render('YOU LOSE!', True, (180, 0, 0))
 
-font2 = font.Font(None, 36)
+# font2 = font.Font(None, 36)
+font2 = font.SysFont('Arial', 36)
 
 # background music
 mixer.init()
